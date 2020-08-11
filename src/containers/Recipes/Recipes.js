a6792fd5f0af9443927a6409994e53ae7246cbb1
Killian Jackson <killianjacksn@gmail.com>
2020-08-10 21:06:36 -0700

    Added scrolling to recipe list

--- src/containers/Recipes/Recipes.js
+++ src/containers/Recipes/Recipes.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import RecipeList from './RecipeList/RecipeList';
+import Aux from '../../components/hoc/Aux/Aux';
 
 const recipes = () => {
   return (
-    <RecipeList />
+    <Aux>
+      <RecipeList />
+    </Aux>
   )
 }
 
